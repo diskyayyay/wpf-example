f4def4b58cfb28b545576a3db6d3c2475a6790cb
diskyayay <supatatdisk@gmail.com>
2022-11-22 17:08:14 +0700

    first commit

---
+++ NotesApp/ViewModel/NotesVM.cs
@@ -0,0 +1,110 @@
+﻿using NotesApp.Model;
+using NotesApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+    public class NotesVM: INotifyPropertyChanged
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Notebook selectedNotebook;
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                NotifyPropertyChanged("SelectedNotebook");
+                getNotes();
+                //TODO: get notes
+            }
+        }
+
+       
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            getNoteBooks();
+        }
+
+        public void CreateNotebook()
+        {
+            Notebook newNotebook = new Notebook()
+            {
+                Name = "New notebook"
+            };
+            
+            DatabaseHelper.Insert(newNotebook);
+            Debug.WriteLine("created");
+            getNoteBooks();
+        }
+
+        public void CreateNote(int notebookId)
+        {
+            Note newNote = new Note()
+            {
+                NotebookId = notebookId,
+                CreatedTime = DateTime.Now,
+                UpdatedTime = DateTime.Now,
+                Title = "New note"
+            };
+
+            DatabaseHelper.Insert(newNote);
+            getNotes();
+        }
+
+        private void getNoteBooks()
+        {
+            var notebooks = DatabaseHelper.Read<Notebook>();
+            Notebooks.Clear();
+
+            foreach(var notebook in notebooks)
+            {
+                Notebooks.Add(notebook);
+            }
+
+        }
+
+        private void getNotes()
+        {
+            if(SelectedNotebook != null)
+            {
+                var notes = DatabaseHelper.Read<Note>().Where(n=> n.NotebookId == SelectedNotebook.Id);
+                Notes.Clear();
+
+                foreach (var note in notes)
+                {
+                    Notes.Add(note);
+                }
+            }
+        }
+
+
+    }
+}
